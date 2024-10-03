484a94e3087b5a2ed8df5948c92f865da81619e2
Prasanna Kante <me210003054@iiti.ac.in>
2024-10-04 01:02:46 +0530

    Add files via upload
    uploaded .ino file

--- allCleaningProcessMotions.ino
+++ allCleaningProcessMotions.ino
@@ -1,47 +1,18 @@
-/* 
-╔══╗──────────╔╗──────╔═╗ ╔═╗╔═╗─────╔╗╔═╗╔═╗─╔╦═══╗
-╚╣╠╝─────────╔╝║──────║╔╝ ║║╚╝║║─────║║║╔╝║║╚╗║╠╗╔╗║
- ║║──╔══╦╗╔╗ ╚╗║──╔══╦╝╚╗ ║╔╗╔╗╠╗─╔╗ ║╚╝╝╔╣╔╗╚╝║║║║║
- ║║──║╔╗║╚╝║──║║──║╔╗╠╗╔╝ ║║║║║║║─║║ ║╔╗║╠╣║╚╗║║║║║║
-╔╣╠╗ ║╔╗║║║║ ╔╝╚╗ ║╚╝║║║──║║║║║║╚═╝║ ║║║╚╣║║─║║╠╝╚╝║
-╚══╝ ╚╝╚╩╩╩╝ ╚══╝ ╚══╝╚╝──╚╝╚╝╚╩═╗╔╝ ╚╝╚═╩╩╝─╚═╩═══╝
- ──────────────────────────────╔═╝║
-───────────────────────────────╚══╝
 
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣄    
-⠄⠄⠄⠄⠄⠄⠄⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⢠⣄⡀
-⠄⠄⠄⠄⠄⠄⢠⣿⣿⣿⠿⢿⣿⣿⣿⣿⣿⡏⠄⠄⢸⣿⣿⡆
-⠄⠄⠄⠄⠄⠄⣿⠟⣻⣿⡿⠛⢻⣿⣿⣿⣿⣷⣄⣠⣾⣿⡇
-⠄⠄⠄⠄⠄⢸⣿⣿⠋⠻⣿⡀⠄⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⠆
-⠄⠄⠄⠄⠄⢸⣿⡿⠄⠄⢻⣧⣀⠄⠄⣸⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣿⡇
-⠄⠄⠄⠄⠄⠸⣿⣇⠄⠄⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁
-⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⠁⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯
-⠄⠄⠄⠄⣀⣀⣤⣿⣿⣿⣿⣿⣶⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿
-⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁
-⠄⠄⠄⠈⢿⣿⡟⠛⠿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆
-⠄⠄⠄⠄⠄⠈⠁⠄⠄⠄⠉⠄⠙⠻⣿⣿⣿⣿⣿⣿⠿⠟⠁
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠛⠿⢟
-─╔═══╦╗╔═╗
-─║╔═╗║║║╔╝
-─║╚═╝║╚╝╝
-─║╔══╣╔╗║
-╔╣║──║║║╚╗
-╚╩╝──╚╝╚═╝
-*/
 int vacuum=5;
 
 // IR sensor 
 int IR1 = 6;
 int IR2 = 7;
 //enable motor 1 and motor 2
-int enA = 8;
+int enA = 12;
 int enB = 13;
 // Motor 1 connections
-int in1 = 9;
+int in1 = 11;
 int in2 = 10;
 // Motor 2 connections
-int in3 = 11;
-int in4 = 12;
+int in3 = 9;
+int in4 = 8;
 //default position
 int pos=-1;
 //-1 => bottom part
@@ -49,17 +20,16 @@ int pos=-1;
 // 1 => upper part
 
 // SET THESE VARIABLES //
-
 //stop motor 1 for this much time after motor 2 does its iteration
-int requiredBuffer= 2000;
+int requiredBuffer= 750;
 //time taken by motor 2 to one way through section
-int timeMotorTwo= 1000;
+int timeMotorTwo= 13000;
 //in how many parts the section has been divided
 //ie. motor One has to go in loop for these many times
-int iterationOfMotorOne= 2;
-int iterationOfMotorTwo= 3;
+int iterationOfMotorOne= 3;
+int iterationOfMotorTwo= 1;
 //how much time we want to run motor One in case of stepwise rotation
-int pitchTimeMotorOne= 1000; 
+int pitchTimeMotorOne= 2500; 
 
 void setup() {
     pinMode(vacuum, OUTPUT);
@@ -84,8 +54,10 @@ void setup() {
 void loop() {
   //initial readings
   pos=-1;
+  //do set up`
   int reading1= digitalRead(IR1);
   int reading2= digitalRead(IR2);
+  delay(5000);
 
   //if any one of the section is dirty then trigger the Util function
   if (reading1 || reading2) motorOneUtils(reading1, reading2);
@@ -93,30 +65,39 @@ void loop() {
 
 //logic for the code, other functions are just agents
 void motorOneUtils (int reading1, int reading2){
+  
+  reading1= digitalRead(IR1);
+  reading2= digitalRead(IR2);
   //run this loop while any one of the section is dirty
   while (reading1 || reading2){
     int from= pos;
     int to= (from==-1 || from==1) ? 0 : (reading1) ? -1 : 1 ;
     //if we want to go stepwise 
     //intialize stepwise as true
+
+    if(pos==-1 && reading1){
+      moveMotorTwo();
+      //stop motor 1 and move motor 2 till del time
+      delay(requiredBuffer);
+    }
+
     bool stepwise= true;
-    
     //skip section2 if it is clean and move to section 1
     if (pos==1 && !reading2) stepwise= false;
     //skip section1 if it is clean and move to section 2
-    else if (pos==-1 && !reading1) stepwise= false; 
-    
+    else if (pos==-1 && !reading1) stepwise= false;
+
     controlMotorOne(from, to, stepwise);
     //take reading again to check if the section is cleaned or not
-    reading1= digitalRead(IR1);
-    reading2= digitalRead(IR2);
-    // if (reading1) reading1= false;
-    // if (reading2) reading2= false;
-    //change the current position
     pos= to;
+    if (pos==0) reading1=false;
+    else if (pos==1) reading2=false;
+    //change the current position
   }
   //after cleaning send the motor to down position again
   controlMotorOne(pos, -1, false);
+
+  delay(5000);
   pos=-1;
 }
 
@@ -125,6 +106,7 @@ void controlMotorOne (int from, int to, bool stepwise){
   int cnt= iterationOfMotorOne;
   //if we want to clean both the sections then double the num of times we want to stop the motor1
   cnt= cnt*(abs(to-from));
+
   //variable that ensures if we want to move motor 1 or not
   bool motion= true;
   while(cnt>0){
@@ -136,6 +118,8 @@ void controlMotorOne (int from, int to, bool stepwise){
     if (stepwise) {
       //false => do not move the motor 1
       moveMotorOne(from, to, !motion);
+      //stop motor1 for some time if in case there is some clash
+      delay(requiredBuffer);
       moveMotorTwo();
       //stop motor 1 and move motor 2 till del time
       delay(requiredBuffer);
@@ -148,6 +132,9 @@ void controlMotorOne (int from, int to, bool stepwise){
 
 //function to give direction to the motor
 void moveMotorOne (int from, int to, bool motion){
+  // full speed of motor 1 with given voltage
+  analogWrite(enA,255);
+
   //if already at the position then dont do anything
   //if !motion => then it indicated we want to stop the motor
   if (from==to || !motion) {
@@ -157,28 +144,37 @@ void moveMotorOne (int from, int to, bool motion){
   }
   //rotate clockwise if we want to go from lower part to upper
   else if (from<to){
+    // analogWrite(enA, 255);
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
   } 
   //rotate anti-clockwise if we want to go from upper part to lower
   else {
+    // analogWrite(enA, 255);
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
   }
 }
 
 void moveMotorTwo() {
-    analogWrite(enB, 255);
+    // analogWrite(enB, 255);
     //vacuum should be active till the motor 2 is moving
     digitalWrite(vacuum, HIGH);
     int cnt= iterationOfMotorTwo;  
+    analogWrite(enB,255);
     //run motor 2, cnt times to and fro
     while (cnt>0){
-      digitalWrite(in3, HIGH);
-      digitalWrite(in4, LOW);
-      delay(timeMotorTwo);
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
+      delay(timeMotorTwo);
+
+      //stop motor for a sec to avoid misleading voltage fluctuations and rotation skip
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      delay(500);
+
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
       delay(timeMotorTwo);
       cnt--;
     }
